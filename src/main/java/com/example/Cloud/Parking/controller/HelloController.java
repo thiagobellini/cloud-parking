08ca5bdb249ebd751a03f5d7281ee86563e85efc
Thiago Bellini <thiagobellinics@hotmail.com>
2022-08-21 18:34:00 -0300

    First commit

---
+++ src/main/java/com/example/Cloud/Parking/controller/HelloController.java
@@ -0,0 +1,7 @@
+package com.example.Cloud.Parking.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+}
---
+++ src/test/java/com/example/Cloud/Parking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cloud.Parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
